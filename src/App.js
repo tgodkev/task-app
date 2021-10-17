e08e4126db065da32c550b54432b4705d333cd92
kevin knight <tgodkev@gmail.com>
2021-10-17 18:09:19 -0400

    qwe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: ''},
+      tasks: [],
+    };
+  }
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter task</label>
+          <input type="text" id="taskInput"/>
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
